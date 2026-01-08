3ea06b6ad1e0cb5c39bddae467ae9596c5171b86
masadnazir1 <masadnazir1@gmail.com>
2026-01-08 22:16:27 +0500

    Add comprehensive logging for forensic and audit purposes

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { errorLoggingMiddleware } from './src/logs/ErrorLoggingMiddleware.js';
 import { requestLoggingMiddleware } from './src/logs/RequestLoggingMiddleware.js';
 import { CorsMiddleware } from './src/middlewares/Cors.Middleware.js';
-// import { DomainAuthMiddleware } from './src/middlewares/Domain.Auth.Middleware.js';
+import { DomainAuthMiddleware } from './src/middlewares/Domain.Auth.Middleware.js';
 
 export class App {
   public app = express();
@@ -11,7 +11,7 @@ export class App {
     this.app.use(express.json());
     this.app.use(requestLoggingMiddleware);
     this.app.use(CorsMiddleware.handle);
-    // this.app.use(DomainAuthMiddleware.handle);
+    this.app.use(DomainAuthMiddleware.handle);
 
     //
     this.app.use(errorLoggingMiddleware);
